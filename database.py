4a8164a86f554dba2ff1f86aab0b61af2f7c2935
﻿Max Floren <maxfloren824@gmail.com>
2026-09-01 20:18:16 -0500

    Update save_goal()

--- database.py
+++ database.py
@@ -1,27 +1,28 @@
 import streamlit as st
+from sqlalchemy import text
 
 def get_connection():
     return st.connection('postgresql', type='sql')
 
 def save_goal(connection, user_id, month, analytic, goal):
-    query = '''
+    query = text('''
         INSERT INTO monthly_goals (user_id, month, analytic, goal)
         VALUES (:user_id, :month, :analytic, :goal)
         ON CONFLICT (user_id, month, analytic)
         DO UPDATE SET goal = EXCLUDED.goal
-    '''
+    ''')
 
-    connection.query(
-        query,
-        params={
-            'user_id': user_id,
-            'month': month,
-            'analytic': analytic,
-            'goal': goal
-        },
-        ttl=0
-    )
-
+    with connection.session as session:
+        session.execute(
+            query,
+            {
+                'user_id': user_id,
+                'month': month,
+                'analytic': analytic,
+                'goal': goal
+            }
+        )
+        session.commit()
 
 def load_goal(connection, user_id, month, analytic):
     query = '''
