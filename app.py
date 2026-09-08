37eefb0e24a23a9ea2aa423a573d07a539bc324e
Max Floren <maxfloren824@gmail.com>
2026-09-07 23:19:18 -0500

    Remove chart axis labels

--- app.py
+++ app.py
@@ -295,13 +295,13 @@ with tab1:
         # commission line chart
         commission_fig = px.line(filtered_df, x='date', y='commission', title='Daily Commission')
         commission_fig.update_traces(line=dict(color='springgreen'))
-        commission_fig.update_layout(xaxis=dict(fixedrange=True), yaxis=(dict(tickprefix='$', fixedrange=True)), dragmode=False)
+        commission_fig.update_layout(xaxis=dict(fixedrange=True, title=None), yaxis=(dict(tickprefix='$', fixedrange=True, title=None)), dragmode=False)
         st.plotly_chart(commission_fig, width='stretch', config={'displayModeBar': False, 'staticPlot': False})
     
         # views line chart
         views_fig = px.line(filtered_df, x='date', y='views', title='Daily Views')
         views_fig.update_traces(line=dict(color='royalblue'))
-        views_fig.update_layout(xaxis=dict(fixedrange=True), yaxis=(dict(fixedrange=True)), dragmode=False)
+        views_fig.update_layout(xaxis=dict(fixedrange=True, title=None), yaxis=(dict(fixedrange=True, title=None)), dragmode=False)
         st.plotly_chart(views_fig, width='stretch', config={'displayModeBar': False, 'staticPlot': False})
 
     # GMV line chart
