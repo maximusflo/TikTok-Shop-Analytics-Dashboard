8d657d0c4280ee2b078f9664b40a8c8d5a86cb81
Max Floren <maxfloren824@gmail.com>
2026-09-07 22:43:14 -0500

    Make line charts static

--- app.py
+++ app.py
@@ -297,12 +297,12 @@ with tab1:
         commission_fig = px.line(filtered_df, x='date', y='commission', title='Daily Commission')
         commission_fig.update_traces(line=dict(color='springgreen'))
         commission_fig.update_layout(yaxis=(dict(tickprefix='$')))
-        st.plotly_chart(commission_fig, width='stretch')
+        st.plotly_chart(commission_fig, width='stretch', config={'displayModeBar': False, 'staticPlot': True})
     
         # views line chart
         views_fig = px.line(filtered_df, x='date', y='views', title='Daily Views')
         views_fig.update_traces(line=dict(color='royalblue'))
-        st.plotly_chart(views_fig, width='stretch')
+        st.plotly_chart(views_fig, width='stretch', config={'displayModeBar': False, 'staticPlot': True})
 
     # GMV line chart
     #    gmv_fig = px.line(filtered_df, x='date', y='gmv', title='Daily GMV')
